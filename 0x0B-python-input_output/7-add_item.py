0ee80c4b67a78327c2ddbc298fd2b51c0d0797e2
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 19:31:44 +0100

    add executable permission

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,8 @@ import os
 
 def main():
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
     # Load existing items from the JSON file if it exists
     items = []
     if os.path.exists('add_item.json'):
@@ -23,6 +24,7 @@ def main():
 
     # Save the updated list back to the JSON file
     save_to_json_file(items, 'add_item.json')
+
 
 if __name__ == "__main__":
     main()
